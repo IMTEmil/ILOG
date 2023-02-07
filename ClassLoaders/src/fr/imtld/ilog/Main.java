a1810bfe6bca9de0129c77a018615881accc7cc3
Emil Pérouse <emil.perouse@etu.imt-nord-europe.fr>
2023-02-07 15:47:16 +0100

    clmaincall

--- ClassLoaders/src/fr/imtld/ilog/Main.java
+++ ClassLoaders/src/fr/imtld/ilog/Main.java
@@ -1,6 +1,7 @@
 package fr.imtld.ilog;
 
-import java.util.Scanner;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class Main {
 	public static void displayClassLoader(Class<?> cls) {
@@ -8,16 +9,24 @@ public class Main {
 		String str = cl==null ? "bootstrap class loader" : cl.getClass().getCanonicalName();
 		System.out.println(str);
 	}
+	public void call(Object o, String namMeth) throws Exception {
+	Class<?> cls = o.getClass();
+	String namClass = cls.getCanonicalName();
+	Method m = cls.getMethod(namMeth);
+	System.out.printf("%s#%s()", namClass, namMeth);
+	Object res = m.invoke(o);
+	if (m.getReturnType() != void.class)
+		System.out.printf(" -> %s", res);
+	System.out.println();
+}
 
 	public void exec(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		String qname = sc.next();
-		Class<?> cls = Class.forName(qname);
-		displayClassLoader(cls);
-		String nameClass = cls.getCanonicalName();
-		System.out.println(nameClass);
-		sc.close();
-	}
+	Class<?> cls = Class.forName(args[0]);
+	Constructor<?> ct = cls.getDeclaredConstructor();
+	Object o = ct.newInstance();
+	call(o, args[1]);
+	call(o, args[2]);
+}
 	
 
 	public static void main(String[] args) throws Exception {
