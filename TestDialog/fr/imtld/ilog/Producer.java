b13ec2851386b6e6d7eba132ebe475b4487d4f19
Emil Pérouse <emil.perouse@etu.imt-nord-europe.fr>
2023-02-20 13:26:29 +0100

    producer

--- TestDialog/fr/imtld/ilog/Producer.java
+++ TestDialog/fr/imtld/ilog/Producer.java
@@ -8,9 +8,15 @@ public class Producer implements Runnable {
 	}
 
 	public void run() {
+		synchronized(this){
+			try {
+				while (fifo.size == 100) wait();	
+			} catch (InterruptedException e) {
+			}
 		for (int i = 0; i < 100; i++) {
 			fifo.append(i);
 		}
 		System.err.println("producer ended");
 	}
+}
 }
