87721bcef2737d86ad73ed714fec15c53ee130a3
Emil Pérouse <emil.perouse@etu.imt-nord-europe.fr>
2023-02-20 10:32:33 +0100

    2loops

--- TestDialog/fr/imtld/ilog/Dialog.java
+++ TestDialog/fr/imtld/ilog/Dialog.java
@@ -6,7 +6,9 @@ public class Dialog implements Runnable {
 		Thread thrCur = Thread.currentThread();
 		String name = thrCur.getName();
 		System.out.printf("%s entering Dialog#run\n", name);
-		// ...
-		System.out.printf("%s leaving Dialog#run\n", name);
+		while (true) {
+			System.out.printf("%s looping in Dialog#run\n", name);
+		}
+//		System.out.printf("%s leaving Dialog#run\n", name);
 	}
 }
--- TestDialog/fr/imtld/ilog/TestDialog.java
+++ TestDialog/fr/imtld/ilog/TestDialog.java
@@ -5,8 +5,11 @@ public class TestDialog {
 		Thread thrCur = Thread.currentThread();
 		String name = thrCur.getName();
 		System.out.printf("%s entering TestDialog#main\n", name);
-                Dialog dlg = new Dialog();
+		Dialog dlg = new Dialog();
 		dlg.run();
-		System.out.printf("%s leaving TestDialog#main\n", name);
+		while (true) {
+			System.out.printf("%s looping in TestDialog#main\n", name);
+		}
+//		System.out.printf("%s leaving TestDialog#main\n", name);
 	}
 }
