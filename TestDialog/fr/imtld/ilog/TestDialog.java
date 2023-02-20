335f8260eb803b48eeaf9a07b62d460a0970f69f
Emil Pérouse <emil.perouse@etu.imt-nord-europe.fr>
2023-02-20 10:36:21 +0100

    2threads

--- TestDialog/fr/imtld/ilog/TestDialog.java
+++ TestDialog/fr/imtld/ilog/TestDialog.java
@@ -1,12 +1,15 @@
 package fr.imtld.ilog;
 
+import java.io.IOException;
+
 public class TestDialog {
 	public static void main(String[] args) {
 		Thread thrCur = Thread.currentThread();
 		String name = thrCur.getName();
 		System.out.printf("%s entering TestDialog#main\n", name);
 		Dialog dlg = new Dialog();
-		dlg.run();
+		Thread thrDlg = new Thread(dlg);
+		thrDlg.start();
 		while (true) {
 			System.out.printf("%s looping in TestDialog#main\n", name);
 		}
